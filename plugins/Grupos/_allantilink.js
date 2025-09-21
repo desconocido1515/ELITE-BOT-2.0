358cfec71e1d268f5a5ee06fec0925f75ae148aa
desconocido <129682184+desconocido1515@users.noreply.github.com>
2025-09-21 15:14:44 -0500

    Update _allantilink.js

--- plugins/Grupos/_allantilink.js
+++ plugins/Grupos/_allantilink.js
@@ -1,120 +1,48 @@
-import fetch from 'node-fetch'  
+const linkRegex = /chat\.whatsapp\.com\/(?:invite\/)?([0-9A-Za-z]{20,24})/i;
+const channelLinkRegex = /whatsapp\.com\/channel\/([0-9A-Za-z]{20,30})/i;
 
-const isLinkTik = /tiktok.com/i 
-const isLinkYt = /youtube.com|youtu.be/i 
-const isLinkTel = /telegram.com|t.me/i 
-const isLinkFb = /facebook.com|fb.me/i 
-const isLinkIg = /instagram.com/i 
-const isLinkTw = /twitter.com|x.com/i 
-const isLinkDc = /discord.com|discord.gg/i 
-const isLinkTh = /threads.net/i 
-const isLinkTch = /twitch.tv/i
-  
-let handler = m => m
-handler.before = async function (m, { conn, isAdmin, isBotAdmin, isOwner, isROwner }) {
-if (!m.isGroup) return 
-if (isAdmin || isOwner || m.fromMe || isROwner || !isBotAdmin) return
+export async function before(m, { conn, isAdmin, isBotAdmin}) {
+  // Ignorar mensajes del propio bot o fuera de grupos
+  if (m.isBaileys || m.fromMe) return true;
+  if (!m.isGroup) return false;
 
-//if (!isAdmin || !isOwner || !isROwner || m.fromMe || !isBotAdmin) return
+  const chat = global.db.data.chats[m.chat];
+  const botSettings = global.db.data.settings[conn.user.jid] || {};
 
-let chat = global.db.data.chats[m.chat]
-let bot = global.db.data.settings[this.user.jid] || {}
-let delet = m.key.participant
-let bang = m.key.id
-let toUser = `${m.sender.split("@")[0]}`
-let aa = toUser + '@s.whatsapp.net'
-    
-const isAntiLinkTik = isLinkTik.exec(m.text)
-const isAntiLinkYt = isLinkYt.exec(m.text)
-const isAntiLinkTel = isLinkTel.exec(m.text)
-const isAntiLinkFb = isLinkFb.exec(m.text)
-const isAntiLinkIg = isLinkIg.exec(m.text)
-const isAntiLinkTw = isLinkTw.exec(m.text)
-const isAntiLinkDc = isLinkDc.exec(m.text)
-const isAntiLinkTh = isLinkTh.exec(m.text)
-const isAntiLinkTch = isLinkTch.exec(m.text)
- 
-if (chat.antiTiktok && isAntiLinkTik) {  
-if (chat.delete) return conn.sendMessage(m.chat, { text: mid.mAdvertencia + mid.mAntiDelete }, { quoted: m })  
-if (isBotAdmin) {
-await conn.reply(m.chat, `${mid.mAdvertencia + mid.mTiktok} *@${toUser}*`, null, { mentions: [aa] })
-await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }})
-let remove = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
-if (remove[0].status === '404') return
-}}
-    
-if (chat.antiYoutube && isAntiLinkYt) {
-if (chat.delete) return conn.sendMessage(m.chat, { text: mid.mAdvertencia + mid.mAntiDelete }, { quoted: m })  
-if (isBotAdmin) {
-await conn.reply(m.chat, `${mid.mAdvertencia + mid.mYoutube} *@${toUser}*`, null, { mentions: [aa] })
-await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }})
-let remove = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
-if (remove[0].status === '404') return
-}}
-    
-if (chat.antiTelegram && isAntiLinkTel) {
-if (chat.delete) return conn.sendMessage(m.chat, { text: mid.mAdvertencia + mid.mAntiDelete }, { quoted: m })   
-if (isBotAdmin) {
-await conn.reply(m.chat, `${mid.mAdvertencia + mid.mTelegram} *@${toUser}*`, null, { mentions: [aa] })
-await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }})
-let remove = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
-if (remove[0].status === '404') return
-}}
-    
-if (chat.antiFacebook && isAntiLinkFb) {
-if (chat.delete) return conn.sendMessage(m.chat, { text: mid.mAdvertencia + mid.mAntiDelete }, { quoted: m })  
-if (isBotAdmin) {
-await conn.reply(m.chat, `${mid.mAdvertencia + mid.mFacebook} *@${toUser}*`, null, { mentions: [aa] })
-await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }})
-let remove = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
-if (remove[0].status === '404') return 
-}}
-    
-if (chat.antiInstagram && isAntiLinkIg) {
-if (chat.delete) return conn.sendMessage(m.chat, { text: mid.mAdvertencia + mid.mAntiDelete }, { quoted: m })  
-if (isBotAdmin) {
-await conn.reply(m.chat, `${mid.mAdvertencia + mid.mInstagram} *@${toUser}*`, null, { mentions: [aa] })
-await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }})
-let remove = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
-if (remove[0].status === '404') return 
-}}
-    
-if (chat.antiTwitter && isAntiLinkTw) {
-if (chat.delete) return conn.sendMessage(m.chat, { text: mid.mAdvertencia + mid.mAntiDelete }, { quoted: m })  
-if (isBotAdmin) {
-await conn.reply(m.chat, `${mid.mAdvertencia + mid.mX} *@${toUser}*`, null, { mentions: [aa] })
-await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }})
-let remove = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
-if (remove[0].status === '404') return
-}}
+  const isGroupLink = linkRegex.exec(m.text);
+  const isChannelLink = channelLinkRegex.exec(m.text);
 
-if (chat.antiDiscord && isAntiLinkDc) {
-if (chat.delete) return conn.sendMessage(m.chat, { text: mid.mAdvertencia + mid.mAntiDelete }, { quoted: m })  
-if (isBotAdmin) {
-await conn.reply(m.chat, `${mid.mAdvertencia + mid.mDiscord} *@${toUser}*`, null, { mentions: [aa] })
-await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }})
-let remove = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
-if (remove[0].status === '404') return
-}}
-
-if (chat.antiThreads && isAntiLinkTh) {
-if (chat.delete) return conn.sendMessage(m.chat, { text: mid.mAdvertencia + mid.mAntiDelete }, { quoted: m })  
-if (isBotAdmin) {
-await conn.reply(m.chat, `${mid.mAdvertencia + mid.mThreads} *@${toUser}*`, null, { mentions: [aa] })
-await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }})
-let remove = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
-if (remove[0].status === '404') return
-}}
-
-if (chat.antiTwitch && isAntiLinkTch) {
-if (chat.delete) return conn.sendMessage(m.chat, { text: mid.mAdvertencia + mid.mAntiDelete }, { quoted: m })  
-if (isBotAdmin) {
-await conn.reply(m.chat, `${mid.mAdvertencia + mid.mTwitch} *@${toUser}*`, null, { mentions: [aa] })
-await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }})
-let remove = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
-if (remove[0].status === '404') return
-}}
-
-return !0
+  // Si antiLink está activado y se detecta un enlace, y el usuario no es admin
+  if (chat.antiLink && (isGroupLink || isChannelLink) &&!isAdmin) {
+    if (!isBotAdmin) {
+      await conn.reply(
+        m.chat,
+        `⚠️ Enlace detectado, pero necesito ser administrador para tomar acción.`,
+        m,
+        { mentions: [m.sender]}
+);
+      return true;
 }
-export default handler
+
+    // Verificar si el enlace es del mismo grupo
+    const thisGroupCode = await conn.groupInviteCode(m.chat);
+    const thisGroupLink = `https://chat.whatsapp.com/${thisGroupCode}`;
+
+    if (m.text.includes(thisGroupLink)) return true; // No expulsar si es el mismo grupo
+
+    // Eliminar el mensaje
+    await conn.sendMessage(m.chat, { delete: m.key});
+
+    // Notificar y expulsar al usuario
+    await conn.reply(
+      m.chat,
+      `⚠️ *Enlace externo detectado*\n\n*@${m.sender.split('@')[0]}* ha sido eliminado por compartir enlaces externos.`,
+      m,
+      { mentions: [m.sender]}
+);
+
+    await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove');
+}
+
+  return true;
+}

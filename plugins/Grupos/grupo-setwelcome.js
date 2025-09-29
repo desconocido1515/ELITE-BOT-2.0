3698549f12d7a2e0abf57c5c5fde6e6063f6d6b3
desconocido <129682184+desconocido1515@users.noreply.github.com>
2025-09-28 19:25:08 -0500

    Update grupo-setwelcome.js

--- plugins/Grupos/grupo-setwelcome.js
+++ plugins/Grupos/grupo-setwelcome.js
@@ -4,12 +4,48 @@ let handler = async (m, { isAdmin, isOwner, args }) => {
     return m.reply('❌ Solo los administradores pueden usar este comando.');
   }
 
+  const textoPredeterminado = `
+✦ ¡Hola!
+Te ayudaré a configurar la bienvenida.
+
+> *PASO 1*: debes saber que al usar este símbolo (@) te ayuda a etiquetar a la persona, mencionar el grupo e incluir la descripción en este grupo.
+> *PASO 2*: Debes usar el comando .tourl para seleccionar la imagen de Bienvenida. 
+
+» @user
+Para etiquetar a la persona.
+» @desc
+Para incluir la descripción del grupo.
+» @group
+Para mencionar el nombre de este grupo.
+» @count
+Para incluir los números de integrantes del grupo.
+
+════════ ⋆★⋆ ════════
+💫 Ejemplo:
+
+.setwelcome Bienvenido @user al mejor grupo @group, siéntete en casa. 💜
+@count 
+@desc
+(Link de la imagen Obligatorio) 
+════════ ⋆★⋆ ════════
+💫 Otros comandos opcionales:
+
+.resetwelcome
+Restablece cambios por defecto. 
+.setbye Selecciona la despedida de tu grupo.
+`;
+
+  // ----------------- Detectar argumentos -----------------
   const text = args.join(' ');
-  if (!text) return m.reply('❗ Uso: .setwelcome mensaje [link de imagen]\nVariables: @user, @group, @count, @desc');
+
+  if (!text) {
+    // Si no ponen texto, enviar la guía
+    return m.reply(textoPredeterminado);
+  }
 
   // ----------------- Detectar link de imagen -----------------
   const match = text.match(/(https?:\/\/\S+\.(jpg|jpeg|png|gif))/i);
-  if (!match) return m.reply('❌ Debes incluir un **link de imagen** al final del mensaje.');
+  if (!match) return m.reply('!Hola humano! ⭐️\n\nTe falta incluir el link de la imagen personalizada.\n\nEn .setwelcome te doy todas las indicaciones.\n\n2023 Elite Bot Global');
 
   const img = match[0];
   const msg = text.replace(match[0], '').trim();
@@ -23,5 +59,5 @@ let handler = async (m, { isAdmin, isOwner, args }) => {
 
 handler.command = /^setwelcome$/i;
 handler.group = true;
-handler.admin = true; // asegura que solo admins puedan usarlo
+handler.admin = true;
 export default handler;

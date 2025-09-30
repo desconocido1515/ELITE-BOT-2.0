3933790be155fa0eb147b6b27812cb83ca4bca9b
desconocido <129682184+desconocido1515@users.noreply.github.com>
2025-09-29 19:45:06 -0500

    Update sticker-emojimix.js

--- plugins/Stickers/sticker-emojimix.js
+++ plugins/Stickers/sticker-emojimix.js
@@ -1,19 +1,8 @@
-import { sticker } from '../../lib/sticker.js'
-let MessageType = (await import(global.baileys)).default
+
+import MessageType from '@whiskeysockets/baileys'
 import fetch from 'node-fetch'
+import { sticker } from '../../lib/sticker.js'
 import fs from "fs"
-let handler = async (m, { conn, text, args, usedPrefix, command }) => {
-if (!args[0]) throw `𝘿𝙀𝘽𝙀 𝘿𝙀 𝙐𝙎𝘼𝙍 𝘿𝙊𝙎 𝙀𝙈𝙊𝙅𝙄𝙎 𝙔 𝙀𝙉 𝙈𝙀𝘿𝙄𝙊 𝙐𝙎𝘼𝙍 𝙀𝙇 *+*\n𝙀𝙅𝙀𝙈𝙋𝙇𝙊\n*${usedPrefix + command}* 😺+😆\n\n𝙔𝙊𝙐 𝙈𝙐𝙎𝙏 𝙐𝙎𝙀 𝙏𝙒𝙊 𝙀𝙈𝙊𝙅𝙄𝙎 𝘼𝙉𝘿 𝙄𝙉 𝙏𝙃𝙀 𝙈𝙄𝘿𝘿𝙇𝙀 𝙐𝙎𝙀 𝙏𝙃𝙀 *+*\n𝙀𝙓𝘼𝙈𝙋𝙇𝙀\n*${usedPrefix + command}* 😼+😁`
-let [emoji1, emoji2] = text.split`+`
-let anu = await fetchJson(`https://tenor.googleapis.com/v2/featured?key=AIzaSyAyimkuYQYF_FXVALexPuGQctUWRURdCYQ&contentfilter=high&media_filter=png_transparent&component=proactive&collection=emoji_kitchen_v5&q=${encodeURIComponent(emoji1)}_${encodeURIComponent(emoji2)}`)
-for (let res of anu.results) {
-let stiker = await sticker(false, res.url, global.packname, global.author)
-conn.sendFile(m.chat, stiker, null, { asSticker: true })
-}}
-handler.help = ['emojimix'].map(v => v + ' emot1|emot2>')
-handler.tags = ['fun']
-handler.command = /^(emojimix|emogimix|combinaremojis|crearemoji|emojismix|emogismix)$/i
-export default handler
 const fetchJson = (url, options) => new Promise(async (resolve, reject) => {
 fetch(url, options)
 .then(response => response.json())
@@ -23,3 +12,18 @@ resolve(json)
 .catch((err) => {
 reject(err)
 })})
+let handler = async (m, { conn, text, args, usedPrefix, command }) => {
+if (!args[0]) return m.reply(`✨ Ejemplo: *${usedPrefix + command}* 😎+🤑`)
+let [emoji, emoji2] = text.split`+`
+let anu = await fetchJson(`https://tenor.googleapis.com/v2/featured?key=AIzaSyAyimkuYQYF_FXVALexPuGQctUWRURdCYQ&contentfilter=high&media_filter=png_transparent&component=proactive&collection=emoji_kitchen_v5&q=${encodeURIComponent(emoji)}_${encodeURIComponent(emoji2)}`)
+for (let res of anu.results) {
+let stiker = await sticker(false, res.url, global.packname, global.author)
+conn.sendFile(m.chat, stiker, null, { asSticker: true }, m)
+}}
+handler.help = ['emojimix *<emoji+emoji>*']
+handler.tags = ['sticker']
+handler.command = ['emojimix'] 
+//handler.limit = 1
+handler.register = false
+
+export default handler
